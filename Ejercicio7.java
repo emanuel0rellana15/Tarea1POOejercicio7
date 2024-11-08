9422cb2d77ebbec019045918461940c77b4b8d4c
Emanuel Orellana <emanuel16orellana@gmail.com>
2024-11-07 21:01:41 -0600

    Tarea1 POO Ejercicio7

---
+++ Ejercicio7.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio7;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+        if(i%2==0){
+            System.out.println("Numero par: " + i);
+        }
+    }
+    }
+    }
+
